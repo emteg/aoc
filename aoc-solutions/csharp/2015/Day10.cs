32789bb2438e65e45f917b7a33e01cfe979cc02c
Matthias Grabowski <matthias.grabowski@theobald-software.com>
2025-12-27 20:58:59 +0100

    Imported 2015 / Day 10

---
+++ aoc-solutions/csharp/2015/Day10.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+
+namespace _2015;
+
+public static class Day10
+{
+    public static string Part1(IEnumerable<string> input)
+    {
+        return LookAndSay(input.First(), 40).Length.ToString();
+    }
+
+    public static string Part1Sample() => Part1(Sample.Lines());
+    
+    public static string Part2(IEnumerable<string> input)
+    {
+        return LookAndSay(input.First(), 50).Length.ToString();
+    }
+    
+    public static string Part2Sample() => Part2(Sample.Lines());
+    
+    private static string LookAndSay(string s, int iterations)
+    {
+        while (iterations > 0)
+        {
+            List<char> chars = [];
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                int howMany = 1;
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[j] != c)
+                        break;
+                    
+                    howMany++;
+                }
+
+                string howManyStr = howMany.ToString();
+                chars.AddRange(howManyStr.ToCharArray());
+                chars.Add(c);
+                i += howMany - 1;
+            }
+
+            string result = new string(chars.ToArray());
+            s = result;
+            iterations--;
+        }
+
+        return s;
+    }
+    
+    private const string Sample = "111221";
+}
